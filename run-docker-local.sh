d9b4710d902fa9c929626e2ac378012600ea1fc1
munslowl <lance.munslow@digital.justice.gov.uk>
2026-01-14 12:13:25 +0000

    Tidy up shell script to launch local Docker

--- run-docker-local.sh
+++ run-docker-local.sh
@@ -1,9 +1,4 @@
 docker run \
-  -e DB_SERVER=localhost:5432 \
-  -e DB_USER=prisoner-pay-api \
-  -e DB_PASS=prisoner-pay-api \
-  -e DB_SERVER=prisoner-pay-api-db:5432 \
-  -e API_BASE_URL_HMPPS_AUTH="https://sign-in-dev.hmpps.service.justice.gov.uk/auth" \
   -e SPRING_DATASOURCE_URL="jdbc:postgresql://prisoner-pay-api-db:5432/prisoner-pay-api-db?sslmode=prefer" \
   -e HMPPS_SQS_LOCALSTACK_URL="http://prisoner-pay-api-localstack:4566" \
   -e SPRING_PROFILES_ACTIVE="local" \
