aaa55b4ecdc930f2f41fb49bd44eea184183d34f
mitchell-dsilva <mitchell.dsilva@justice.gov.uk>
2026-02-04 17:31:25 +0000

    Amended integration tests to make them fail safe

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerpayapi/integration/PayRateIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerpayapi/integration/PayRateIntegrationTest.kt
@@ -1,84 +1,130 @@
 package uk.gov.justice.digital.hmpps.prisonerpayapi.integration
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.whenever
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.context.jdbc.Sql
 import uk.gov.justice.digital.hmpps.prisonerpayapi.dto.response.PayRateDto
 import uk.gov.justice.digital.hmpps.prisonerpayapi.helper.RISLEY_PRISON_CODE
 import uk.gov.justice.digital.hmpps.prisonerpayapi.jpa.entity.PayStatusType
+import java.time.Instant
 import java.time.LocalDate
 
 class PayRateIntegrationTest : IntegrationTestBase() {
+
+  private lateinit var today: LocalDate
+
+  @BeforeEach
+  fun clockSetup() {
+    whenever(clock.instant()).thenReturn(Instant.parse("2026-02-01T02:00:00.00Z"))
+    today = LocalDate.now(clock)
+  }
+
   @Nested
   @DisplayName("Get pay rates")
   inner class GetPayRates {
+
     @Test
     @Sql("classpath:sql/pay-rates/pay-rates-with-start-dates-in-past-and-future.sql")
     fun `should return latest past pay rate and all future rates`() {
-      getCurrentAndFuturePayRates().successList<PayRateDto>().let {
-        assertThat(it).hasSize(3)
-        assertThat(it.map { it.prisonCode }).containsOnly("RSI")
-        assertThat(it.map { it.startDate }).containsExactly(
-          LocalDate.of(2026, 1, 15),
-          LocalDate.of(2026, 2, 10),
-          LocalDate.of(2026, 2, 20),
-        )
-        assertThat(it.map { it.rate }).containsExactly(100, 80, 110)
-        assertThat(it.map { it.type }).containsOnly(PayStatusType.LONG_TERM_SICK)
-        assertThat(it.map { it.createdBy }).containsOnly("USER1")
-      }
+      getCurrentAndFuturePayRates(prisonCode = RISLEY_PRISON_CODE)
+        .successList<PayRateDto>()
+        .let { payRates ->
+          assertThat(payRates).hasSize(3)
+
+          payRates.forEach { payRate ->
+            assertThat(payRate.prisonCode).isEqualTo("RSI")
+            assertThat(payRate.type).isEqualTo(PayStatusType.LONG_TERM_SICK)
+            assertThat(payRate.createdBy).isEqualTo("USER1")
+          }
+
+          val pastRates = payRates.filter { !it.startDate.isAfter(today) }
+          val futureRates = payRates.filter { it.startDate.isAfter(today) }
+
+          pastRates.single().let {
+            assertThat(it.startDate).isEqualTo(LocalDate.of(2026, 1, 15))
+            assertThat(it.rate).isEqualTo(100)
+          }
+
+          assertThat(futureRates.map { it.startDate }).containsExactly(
+            LocalDate.of(2026, 2, 10),
+            LocalDate.of(2026, 2, 20),
+          )
+          assertThat(futureRates.map { it.rate }).containsExactly(80, 110)
+        }
     }
 
     @Test
     @Sql("classpath:sql/pay-rates/pay-rates-with-start-dates-only-in-future.sql")
     fun `should return only future pay rates when no past pay rates exist`() {
-      getCurrentAndFuturePayRates(prisonCode = RISLEY_PRISON_CODE).successList<PayRateDto>().let {
-        assertThat(it).hasSize(3)
-        assertThat(it.map { it.prisonCode }).containsOnly("RSI")
-        assertThat(it.map { it.startDate }).containsExactly(
-          LocalDate.of(2026, 2, 20),
-          LocalDate.of(2026, 2, 26),
-          LocalDate.of(2026, 3, 10),
-        )
-        assertThat(it.map { it.rate }).containsExactly(75, 65, 99)
-        assertThat(it.map { it.type }).containsOnly(PayStatusType.LONG_TERM_SICK)
-        assertThat(it.map { it.createdBy }).containsOnly("USER2")
-      }
+      getCurrentAndFuturePayRates(prisonCode = RISLEY_PRISON_CODE)
+        .successList<PayRateDto>()
+        .let { futureRates ->
+          assertThat(futureRates).allMatch { it.startDate.isAfter(today) }
+
+          futureRates.forEach { payRate ->
+            assertThat(payRate.prisonCode).isEqualTo("RSI")
+            assertThat(payRate.type).isEqualTo(PayStatusType.LONG_TERM_SICK)
+            assertThat(payRate.createdBy).isEqualTo("USER2")
+          }
+
+          assertThat(futureRates.map { it.startDate }).containsExactly(
+            LocalDate.of(2026, 2, 20),
+            LocalDate.of(2026, 2, 26),
+            LocalDate.of(2026, 3, 10),
+          )
+
+          assertThat(futureRates.map { it.rate }).containsExactly(75, 65, 99)
+        }
     }
 
     @Test
     @Sql("classpath:sql/pay-rates/pay-rates-with-start-dates-only-in-past.sql")
     fun `should return latest past pay rate when no future pay rates exist`() {
-      getCurrentAndFuturePayRates(prisonCode = RISLEY_PRISON_CODE).successList<PayRateDto>().let {
-        assertThat(it).hasSize(1)
-        assertThat(it.map { it.prisonCode }).containsOnly("RSI")
-        assertThat(it.map { it.startDate }).containsOnly(
-          LocalDate.of(2026, 1, 15),
-        )
-        assertThat(it.map { it.rate }).containsOnly(100)
-        assertThat(it.map { it.type }).containsOnly(PayStatusType.LONG_TERM_SICK)
-        assertThat(it.map { it.createdBy }).containsOnly("USER1")
-      }
+      getCurrentAndFuturePayRates(prisonCode = RISLEY_PRISON_CODE)
+        .successList<PayRateDto>()
+        .single()
+        .let { payRate ->
+          assertThat(payRate.prisonCode).isEqualTo("RSI")
+          assertThat(payRate.type).isEqualTo(PayStatusType.LONG_TERM_SICK)
+          assertThat(payRate.createdBy).isEqualTo("USER1")
+          assertThat(payRate.startDate).isEqualTo(LocalDate.of(2026, 1, 15))
+        }
     }
 
     @Test
     @Sql("classpath:sql/pay-rates/pay-rates-with-start-dates-in-past-today-and-future.sql")
     fun `should return current (today’s) and future pay rates when past, current, and future pay rates exist`() {
-      getCurrentAndFuturePayRates(prisonCode = RISLEY_PRISON_CODE).successList<PayRateDto>().let {
-        assertThat(it).hasSize(2)
-        assertThat(it.map { it.prisonCode }).containsOnly("RSI")
-        assertThat(it.map { it.startDate }).containsExactly(
-          LocalDate.now(),
-          LocalDate.of(2026, 2, 10),
-        )
-        assertThat(it.map { it.rate }).containsExactly(100, 80)
-        assertThat(it.map { it.type }).containsOnly(PayStatusType.LONG_TERM_SICK)
-        assertThat(it.map { it.createdBy }).containsOnly("USER1")
-      }
+      getCurrentAndFuturePayRates(prisonCode = RISLEY_PRISON_CODE)
+        .successList<PayRateDto>()
+        .let { payRates ->
+          assertThat(payRates).hasSize(3)
+
+          payRates.forEach { payRate ->
+            assertThat(payRate.prisonCode).isEqualTo("RSI")
+            assertThat(payRate.type).isEqualTo(PayStatusType.LONG_TERM_SICK)
+            assertThat(payRate.createdBy).isEqualTo("USER1")
+          }
+
+          val pastRates = payRates.filter { !it.startDate.isAfter(today) }
+          val futureRates = payRates.filter { it.startDate.isAfter(today) }
+
+          pastRates.single().let {
+            assertThat(it.startDate).isEqualTo(LocalDate.of(2026, 2, 1))
+            assertThat(it.rate).isEqualTo(120)
+          }
+
+          assertThat(futureRates.map { it.startDate }).containsExactly(
+            LocalDate.of(2026, 2, 10),
+            LocalDate.of(2026, 2, 20),
+          )
+          assertThat(futureRates.map { it.rate }).containsExactly(80, 100)
+        }
     }
 
     @Test
@@ -88,12 +134,17 @@ class PayRateIntegrationTest : IntegrationTestBase() {
 
     @Test
     fun `returns unauthorized when no bearer token`() {
-      getCurrentAndFuturePayRates(includeBearerAuth = false, prisonCode = RISLEY_PRISON_CODE).fail(HttpStatus.UNAUTHORIZED)
+      getCurrentAndFuturePayRates(
+        includeBearerAuth = false,
+        prisonCode = RISLEY_PRISON_CODE,
+      ).fail(HttpStatus.UNAUTHORIZED)
     }
 
     @Test
     fun `returns forbidden when role is incorrect`() {
-      getCurrentAndFuturePayRates(roles = listOf("ROLE_NO_PERMISSIONS"), prisonCode = RISLEY_PRISON_CODE).fail(HttpStatus.FORBIDDEN)
+      getCurrentAndFuturePayRates(roles = listOf("ROLE_NO_PERMISSIONS"), prisonCode = RISLEY_PRISON_CODE).fail(
+        HttpStatus.FORBIDDEN,
+      )
     }
   }
 
--- src/test/resources/sql/pay-rates/pay-rates-with-start-dates-in-past-today-and-future.sql
+++ src/test/resources/sql/pay-rates/pay-rates-with-start-dates-in-past-today-and-future.sql
@@ -5,10 +5,13 @@ insert into pay_rate(prison_code, type, start_date, rate, created_date_time, cre
 values ('RSI', 'LONG_TERM_SICK', '2026-01-29', 120, now(), 'USER1');
 
 insert into pay_rate(prison_code, type, start_date, rate, created_date_time, created_by)
-values ('BCI', 'LONG_TERM_SICK', now(), 120, now(), 'USER1');
+values ('RSI', 'LONG_TERM_SICK', '2026-02-01', 120, now(), 'USER1');
 
 insert into pay_rate(prison_code, type, start_date, rate, created_date_time, created_by)
-values ('RSI', 'LONG_TERM_SICK', now(), 100, now(), 'USER1');
+values ('RSI', 'LONG_TERM_SICK', '2026-02-20', 100, now(), 'USER1');
 
 insert into pay_rate(prison_code, type, start_date, rate, created_date_time, created_by)
 values ('RSI', 'LONG_TERM_SICK', '2026-02-10', 80, now(), 'USER1');
+
+insert into pay_rate(prison_code, type, start_date, rate, created_date_time, created_by)
+values ('BCI', 'LONG_TERM_SICK', '2026-02-10', 85, now(), 'USER1');
