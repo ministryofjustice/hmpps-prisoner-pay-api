03306e4c1512984127ddf077ba59a830c9aa7653
mitchell-dsilva <mitchell.dsilva@justice.gov.uk>
2026-02-04 19:26:27 +0000

    Amended test to use scope functions

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerpayapi/jpa/entity/PayRateTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerpayapi/jpa/entity/PayRateTest.kt
@@ -8,24 +8,26 @@ import java.time.LocalDateTime
 class PayRateTest {
   @Test
   fun `should create a PayRate entity with expected values`() {
-    val startDate = LocalDate.of(2026, 1, 27)
-    val createdDateTime = LocalDateTime.of(2026, 1, 27, 10, 0)
+    val expectedStartDate = LocalDate.of(2026, 1, 27)
+    val expectedCreatedDateTime = LocalDateTime.of(2026, 1, 27, 10, 0)
 
     val payRate = PayRate(
-      prisonCode = "BCI",
+      prisonCode = "RSI",
       type = PayStatusType.LONG_TERM_SICK,
-      startDate = LocalDate.of(2026, 1, 27),
+      startDate = expectedStartDate,
       rate = 90,
       createdBy = "TEST_USER",
-      createdDateTime = LocalDateTime.of(2026, 1, 27, 10, 0),
+      createdDateTime = expectedCreatedDateTime
     )
 
-    assertThat(payRate.prisonCode).isEqualTo("BCI")
-    assertThat(payRate.type).isEqualTo(PayStatusType.LONG_TERM_SICK)
-    assertThat(payRate.startDate).isEqualTo(startDate)
-    assertThat(payRate.rate).isEqualTo(90)
-    assertThat(payRate.createdBy).isEqualTo("TEST_USER")
-    assertThat(payRate.createdDateTime).isEqualTo(createdDateTime)
+    with(payRate) {
+      assertThat(prisonCode).isEqualTo("RSI")
+      assertThat(type).isEqualTo(PayStatusType.LONG_TERM_SICK)
+      assertThat(startDate).isEqualTo(expectedStartDate)
+      assertThat(rate).isEqualTo(90)
+      assertThat(createdBy).isEqualTo("TEST_USER")
+      assertThat(createdDateTime).isEqualTo(expectedCreatedDateTime)
+    }
   }
 
   @Test
