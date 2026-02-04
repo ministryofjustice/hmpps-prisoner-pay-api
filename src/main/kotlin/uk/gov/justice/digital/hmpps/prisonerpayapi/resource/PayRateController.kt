e29e99829341c8b4863780569cce2a586dc292be
mitchell-dsilva <mitchell.dsilva@justice.gov.uk>
2026-02-04 18:46:47 +0000

    Added back response status anotation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerpayapi/resource/PayRateController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerpayapi/resource/PayRateController.kt
@@ -7,11 +7,13 @@ import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.prisonerpayapi.dto.response.PayRateDto
 import uk.gov.justice.digital.hmpps.prisonerpayapi.service.PayRateService
@@ -26,6 +28,7 @@ class PayRateController(
 ) {
   @GetMapping("/prison/{prisonCode}")
   @PreAuthorize("hasRole('ROLE_PRISONER_PAY__PRISONER_PAY_ORCHESTRATOR_API')")
+  @ResponseStatus(HttpStatus.OK)
   @Operation(
     summary = "Retrieve all current and future pay rates by prison code",
     responses = [
